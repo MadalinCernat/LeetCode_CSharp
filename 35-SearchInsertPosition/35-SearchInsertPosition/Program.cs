39e219f12fc43ca414c07a01a36585421241268e
MadalinCernat <madalin.cernat02@gmail.com>
2022-07-30 17:00:02 +0300

    added solution for problem 35

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] nums = { 1, 3, 5, 6 };
+Console.WriteLine(new Solution().SearchInsert(nums, 0));
+
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int l = 0, r = nums.Length - 1, m, poz = 0;
+        while (l <= r)
+        {
+            m = (l + r) / 2;
+            if (target < nums[m])
+            {
+                r = m - 1;
+                poz = r + 1;
+            }
+            else if (target > nums[m])
+            {
+                l = m + 1;
+                poz = l;
+            }
+            else
+            {
+                return m;
+            }
+        }
+        return poz;
+    }
+}
